35b2d4999bd558a4167aec117aa77a717b5fd774
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-05-13 20:58:24 -0300

    feat: refatorar função de filtragem de livros e exibir valor total na tela

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,12 +1,3 @@
-// const botoes = document.querySelectorAll('.btn');
-// botoes.forEach(btn => btn.addEventListener('click',filtrarLivros));
-
-// function filtrarLivros(){
-//     const elementoBtn = document.getElementById(this.id)
-//     const categoria = elementoBtn.value;
-//     let livrosFiltrados = categoria == 'disponivel'? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria  == categoria)
-//     exibirOsLivrosNaTela(livrosFiltrados)
-// }
 const botoes = document.querySelectorAll('.btn')
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
@@ -14,8 +5,25 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
         let livrosFiltrados = categoria == 'disponivel' ? 
-        livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+        fitrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        exibirValorTotalDosLivrosNaTela()
+    }else {
 
+    }
 }
-    
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function fitrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosNaTela(){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML=
+    `<div class="livros__disponiveis">
+       <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>`
+}    
